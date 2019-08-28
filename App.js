02d1b339938c8b6a93cb164e2677ddfbf9659167
Hamzah Al Ghamdi <info@hdb.com.sa>
2019-08-28 12:39:31 +0300

    save

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default class App extends React.Component {
   render() {
@@ -7,7 +7,7 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <Text>Run sh deploy2.sh</Text>
         <Text>Relaunch your app once the update is deployed</Text>
-        <Text>Your new changes should reflect now</Text>
+        <Text>Hamzah is Power</Text>
       </View>
     );
   }
@@ -16,8 +16,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
